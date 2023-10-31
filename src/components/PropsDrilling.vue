84d35a8b476adf25625f4333af57e48ee72928ba
waylon <waylong@wangxutech.com>
2023-10-31 18:05:34 +0800

    feat: provide/inject

---
+++ src/components/PropsDrilling.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { inject, computed } from 'vue'
+
+// inject 依赖
+const { location, updateLocation } = inject('location')
+console.log(location, updateLocation)
+const value = computed({
+  get() {
+    return location.value
+  },
+  set(newVal: string) {
+    updateLocation(newVal)
+  }
+})
+</script>
+
+<template>
+  <input class="block black" v-model="value" />
+</template>
+
+<style scoped lang="scss">
+.block {
+  display: block;
+}
+.black {
+  color: black;
+}
+</style>
